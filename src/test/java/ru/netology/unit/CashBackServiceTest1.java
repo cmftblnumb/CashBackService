b39ffe6efb30810167431a76b116648965cf1a4a
Kristina P <cmftblnumb@gmail.com>
2025-01-23 12:01:11 -0500

    junit4 added

---
+++ src/test/java/ru/netology/unit/CashBackServiceTest1.java
@@ -0,0 +1,35 @@
+package ru.netology.unit;
+
+import static org.testng.Assert.*;
+import org.junit.Test;
+import org.testng.Assert;
+
+public class CashBackServiceTest1 {
+
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashBackService service = new CashBackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashBackService service = new CashBackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn500IfAmountIs1500() {
+        CashBackService service = new CashBackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+}
